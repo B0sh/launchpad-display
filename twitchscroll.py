3765ff179a870675147667436c1c15b8f037ee7c
B0sh <b0shbbl@hotmail.com>
2020-05-30 15:15:31 -0500

    Revert "Fix twitch chat support with new twitch library"
    This reverts commit 7aa9483dd8b70f2f3b9d3367e61b0d34fb4f1443.

--- twitchscroll.py
+++ twitchscroll.py
@@ -3,10 +3,11 @@ from transition import *
 import math
 
 class TwitchScroll:
-    def __init__(self, message):
+    def __init__(self, message, previous_display):
         self.delay = 0.05
         self.position = 0
         self.message = message
+        self.previous_display = previous_display
         unloadDisplay()
             
         self.display = renderFont("  " + message + "   ")
@@ -19,9 +20,9 @@ class TwitchScroll:
             self.position += 1
             time.sleep(self.delay)
 
-    def is_completed(self):
+    def finished(self):
         if self.position == -1 or self.position == getLengthOfDisplay(self.display):
-            return True
+            return self.previous_display
         else:
             return False
         
